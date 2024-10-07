b9bf13ecaa4a9106c730e317b3fb12de5623c123
dru0pa <dru0pa@gmail.com>
2024-10-07 08:28:31 +0200

    Update Create-Recon-Logon.sh
    Updated the skript

--- Create-Recon-Logon.sh
+++ Create-Recon-Logon.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-#Quick script to create users for CyberArk Linux for the Logon Account and the recon Account
-#The recon account is also added to the wheel group to managed reconsile for the Linux server.
+# This Script will create user for the CyberArk
+# The accounts created are <hostname>-recon and <hostname>-logon
 
-# Add the first user
-useradd logon
-echo "Set password for logon:"
-passwd logon
 
-# Add the second user and add to wheel group
-useradd recon
-usermod -aG wheel recon
-echo "Set password for recon:"
-passwd recon
+# Get the hostname of the server.
+hostname=$(hostname -s)
+
+# Create the users.
+echo "Create user  $hostname-recon..."
+sudo useradd "$hostname-recon"
+echo "Create user $hostname-logon..."
+sudo useradd "$hostname-logon"
+
+# Set the passwords for the users.
+echo "Setting password for $hostname-recon..."
+sudo passwd "$hostname-recon"
+echo "Setting password for $hostname-logon..."
+sudo passwd "$hostname-logon"
+
+# Show the accounts created.
+echo "Accounts created:"
+echo " - $hostname-recon"
+echo " - $hostname-logon"
+
+# Add the hostname-recon user to the wheel group.
+echo "Adding the $hostname-recon to wheel group"
+sudo usermod -aG wheel "$hostname-recon"
